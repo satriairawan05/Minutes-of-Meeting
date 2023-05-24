4679ff066ed93f273b46ae2010eb0d140877f2ca
satriairawan05_ <satriai418@gmail.com>
2023-05-24 09:20:07 +0800

    Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin.suemeru@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('suemeru'), // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
