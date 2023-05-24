1514293d5e2bf48c28e36bc80f1856df8205388f
satriairawan05_ <satriai418@gmail.com>
2023-05-24 10:14:51 +0800

    RoleSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\RolesAndPermissionsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,7 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            UserSeeder::class
+            // RolesAndPermissionsSeeder::class
+            UserSeeder::class,
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
