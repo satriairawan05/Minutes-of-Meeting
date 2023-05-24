656b843d9024205b68fbf2e5447b97d866855fec
WTRois <129079350+WTRois@users.noreply.github.com>
2023-05-24 09:31:37 +0800

    halo

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@suemerugrup.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('suemeru'), // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
