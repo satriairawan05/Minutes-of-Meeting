7fc6df0491eacc0b309f48fc10c8da5c94f092e7
Satria Irawan <satriai418@gmail.com>
2023-05-26 10:33:45 +0800

    fix backend meetdetails_

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Model::unguard();
     }
 
     /**
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::unguard();
+        //
     }
 }
--- database/migrations/2023_05_25_024931_create_meet_details_table.php
+++ database/migrations/2023_05_25_024931_create_meet_details_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('meet_details', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('project');
             $table->string('tracker');
             $table->string('subject');
