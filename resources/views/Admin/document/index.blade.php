7cef8cf2b7a55f00996f5a8463c833c83a802240
Satria Irawan <satriai418@gmail.com>
2023-05-27 11:42:10 +0800

    fix views Document

--- resources/views/Admin/document/index.blade.php
+++ resources/views/Admin/document/index.blade.php
@@ -1,12 +1,12 @@
 @extends('admin.layout.dashboard')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            {{ __('List of Document') }}
-        </div>
-        <div class="card-body">
-            <table class="" c></table>
-        </div>
-    </div>
+<div class="card">
+  <div class="card-header">
+    {{ __('List of Document') }}
+  </div>
+  <div class="card-body">
+    <!-- DataTables in Here -->
+  </div>
+</div>
 @endsection
