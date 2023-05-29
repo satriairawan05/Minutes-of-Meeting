4ea23492970a8b26d519a95e4e0fc2cb6e088c7b
Satria Irawan <satriai418@gmail.com>
2023-05-29 09:09:32 +0800

    add foreign

--- database/migrations/2023_05_25_024931_create_meet_details_table.php
+++ database/migrations/2023_05_25_024931_create_meet_details_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('issues', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->foreignId('meet_id');
             $table->string('project',50);
             $table->string('tracker',50);
             $table->string('subject',100);
