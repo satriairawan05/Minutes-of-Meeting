046e6dd9acc040d609e43d3b9473e77724d74b27
Satria Irawan <satriai418@gmail.com>
2023-06-05 08:07:01 +0800

    update web.php & doc

--- routes/web.php
+++ routes/web.php
@@ -19,35 +19,35 @@ use App\Http\Controllers\UserManagementController;
 |
 */
 
-Route::get('/', function () {
-    return view('layout.dashboard');
-})->name('dashboard');
-
-Route::resource('user',UserManagementController::class);
-Route::resource('departemen',DepartemenController::class);
-
-Route::resource('meet', MeetController::class);
-Route::resource('issue', IssueController::class);
-Route::resource('document',DocumentController::class);
-
-// Store data Issue ke Document
-Route::get('issue/{issue}/doc', [DocumentController::class, 'showForm'])->name('issueDoc.form');
-
-// Update data Issue ke Document yang sudah ada
-Route::get('issue/{issue}/doc/{doc}/edit',[DocumentController::class, 'editForm'])->name('issue.document.edit');
-Route::put('issue/{issue}/doc',[DocumentController::class, 'updateForm'])->name('issueDoc.update');
-
-
-Route::get('preference', function(){
-    return view('pref.index');
-})->name('preference');
-
 Route::middleware(['guest'])->group(function () {
-    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login.form');
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('login', [LoginController::class, 'loginStore'])->name('login.store');
 });
 
 Route::middleware(['auth'])->group(function () {
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('/', function () {
+        return view('layout.dashboard');
+    })->name('dashboard');
+
+    Route::resource('user',UserManagementController::class);
+    Route::resource('departemen',DepartemenController::class);
+
+    Route::resource('meet', MeetController::class);
+    Route::resource('issue', IssueController::class);
+    Route::resource('document',DocumentController::class);
+
+    // Store data Issue ke Document
+    Route::get('issue/{issue}/doc', [DocumentController::class, 'showForm'])->name('issueDoc.form');
+
+    // Update data Issue ke Document yang sudah ada
+    Route::get('issue/{issue}/doc/{doc}/edit',[DocumentController::class, 'editForm'])->name('issue.document.edit');
+    Route::put('issue/{issue}/doc',[DocumentController::class, 'updateForm'])->name('issueDoc.update');
+
+
+    Route::get('preference', function(){
+        return view('pref.index');
+    })->name('preference');
 });
 
