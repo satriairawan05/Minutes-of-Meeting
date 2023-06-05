7df400c4e4109122771ba62d7e1b1d8357cff17e
Satria Irawan <satriai418@gmail.com>
2023-06-05 17:13:25 +0800

    fix roles

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -14,7 +14,7 @@ class GroupController extends Controller
      */
     public function index()
     {
-        return view('group.index',[
+        return view('group.index', [
             'groups' => Group::get()
         ]);
     }
@@ -24,7 +24,7 @@ class GroupController extends Controller
      */
     public function create()
     {
-        return view('group.create',[
+        return view('group.create', [
             'page_distincts' => Page::distinct('page_name')->get('page_name'),
             'pages' => Page::all()
         ]);
@@ -33,29 +33,22 @@ class GroupController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Group $group, Page $page, GroupPage $groupPage)
     {
-        // return dd($request->all());
         try {
-            $validate = $request->validate([
-                'group_name' => 'required|min:5|max:255',
-            ]);
-
-            if($validate == $request){
-                $group = new Group;
-                $group->group_name = $request->group_name;
-                $group->save();
-                $pages = Page::all();
-                foreach($pages as $page){
-                    $groupPage = new GroupPage;
-                    $groupPage->page_id = $page->page_id;
-                    $groupPage->group_id = $group->group_id;
-                    $groupPage->action = $request->input($page->page_name.$page->action) == "on" ? 1 : 0;
-                    $groupPage->save();
-                }
+            $group = new Group;
+            $group->group_name = $request->group_name;
+            $group->save();
+            $pages = Page::all();
+            foreach ($pages as $page) {
+                $groupPage = new GroupPage;
+                $groupPage->page_id = $page->page_id;
+                $groupPage->group_id = $group->group_id;
+                $groupPage->access = $request->input($page->page_name . $page->action) == "on" ? 1 : 0;
+                $groupPage->save();
             }
 
-            return redirect()->to('group')->with('success','Added Role Successfully!')->withInput();
+            return redirect()->to('group')->with('success', 'Added Role Successfully!')->withInput();
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
@@ -74,7 +67,7 @@ class GroupController extends Controller
      */
     public function edit(Group $group)
     {
-        return view('group.edit',[
+        return view('group.edit', [
             'group' => $group
         ]);
     }
@@ -86,16 +79,16 @@ class GroupController extends Controller
     {
         try {
             $rules = [
-                'group_name' => ['required','max:255']
+                'group_name' => ['required', 'max:255']
             ];
 
             $validate = $request->validate($rules);
 
-            Group::where('id',$group->id)->update($validate);
+            Group::where('id', $group->id)->update($validate);
 
-            return redirect('group')->with('success','Updated Roles Successfully!');
+            return redirect('group')->with('success', 'Updated Roles Successfully!');
         } catch (\Throwable $th) {
-            return redirect('group')->with('failed',$th->getMessage());
+            return redirect('group')->with('failed', $th->getMessage());
         }
     }
 
@@ -107,9 +100,9 @@ class GroupController extends Controller
         try {
             Group::destroy($group->id);
 
-            return redirect('group')->with('success','Deleted Roles Successfully!');
+            return redirect('group')->with('success', 'Deleted Roles Successfully!');
         } catch (\Throwable $th) {
-            return redirect('group')->with('failed',$th->getMessage());
+            return redirect('group')->with('failed', $th->getMessage());
         }
     }
 }
