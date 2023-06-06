f3f4af04b8a023f286974c18fa3743a9da8dcce3
alsyauqi <ali.subterror@gmail.com>
2023-06-06 10:32:14 +0800

    fix back, submit & header button deletion
    sat bang sat

--- resources/views/meet/create.blade.php
+++ resources/views/meet/create.blade.php
@@ -14,19 +14,6 @@
                         <li class="breadcrumb-item active" aria-current="page">Add Meet</li>
                     </ol>
                 </div>
-                <div class="d-flex">
-                    <div class="justify-content-center">
-                        <button type="button" class="btn btn-white btn-icon-text my-2 mr-2">
-                            <i class="fe fe-download mr-2"></i> Import
-                        </button>
-                        <button type="button" class="btn btn-white btn-icon-text my-2 mr-2">
-                            <i class="fe fe-filter mr-2"></i> Filter
-                        </button>
-                        <button type="button" class="btn btn-primary my-2 btn-icon-text">
-                            <i class="fe fe-download-cloud mr-2"></i> Download Report
-                        </button>
-                    </div>
-                </div>
             </div>
             <!-- End Page Header -->
 
