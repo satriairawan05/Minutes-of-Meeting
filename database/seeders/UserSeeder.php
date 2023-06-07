bdb2009e78a979bff9be00fe9b549288d8b6cbb1
Satria Irawan <satriai418@gmail.com>
2023-06-07 22:02:03 +0800

    add account for Production Mode

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,10 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
         User::create([
             'name' => 'Super Admin',
             'email' => 'admin@bsscoal.com',
-            'password' => bcrypt('suemeru'),
+            'password' => bcrypt('bsscoal'),
             'email_verified_at' => now(),
             'group_id' => 1,
             'departemen' => '',
@@ -27,7 +28,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'PIC',
             'email' => 'pic@bsscoal.com',
-            'password' => bcrypt('suemeru'),
+            'password' => bcrypt('bsscoal'),
             'email_verified_at' => now(),
             'group_id' => 2,
             'departemen' => '',
@@ -35,13 +36,104 @@ class UserSeeder extends Seeder
         ]);
 
         User::create([
+            'name' => 'Engginering',
+            'email' => 'engginering@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'Engginering',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'HSE',
+            'email' => 'hse@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'HSE',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Coal & Berging',
+            'email' => 'cnb@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'Coal & Berging',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'FAT',
+            'email' => 'fat@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'FAT',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'HRGA',
+            'email' => 'hrga@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'HRGA',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
             'name' => 'IT',
             'email' => 'it@bsscoal.com',
-            'password' => bcrypt('suemeru'),
+            'password' => bcrypt('bsscoal'),
             'email_verified_at' => now(),
             'group_id' => 3,
             'departemen' => 'IT',
             'remember_token' => Str::random(10),
         ]);
+
+        User::create([
+            'name' => 'Logistic',
+            'email' => 'logistic@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'Logistic',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Plant',
+            'email' => 'plant@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'Plant',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Production',
+            'email' => 'production@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'Production',
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'purcashing',
+            'email' => 'purcashing@bsscoal.com',
+            'password' => bcrypt('bsscoal'),
+            'email_verified_at' => now(),
+            'group_id' => 3,
+            'departemen' => 'purcashing',
+            'remember_token' => Str::random(10),
+        ]);
+        //'Engginering','HSE','Coal & Berging','FAT','HRGA','IT','LOGISTIC','PLANT','PRODUCTION','PURCASHING'
     }
 }
