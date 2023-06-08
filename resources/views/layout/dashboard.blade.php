ab07fe3f8801840203bd6799ad8d3c599a422ed7
alsyauqi <ali.subterror@gmail.com>
2023-06-08 16:33:32 +0800

    form edit & create, dashboard icons

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -31,7 +31,6 @@
                                             <div class="offset-xl-3 offset-sm-6 col-xl-8 col-sm-6 col-12 img-bg ">
                                                 <h4 class="d-flex  mb-3">
                                                     <span class="font-weight-bold text-white ">
-                                                        {{-- bangsat --}}
                                                         Assalamualaikum {{ auth()->user()->name }}!</span>
                                                 </h4>
                                                 <p class="tx-white-7 mb-1">You have two projects to finish, you
@@ -46,7 +45,6 @@
                             </div>
                         </div>
                         <!--Row -->
-
                         <!--Row-->
                         <div class="row row-sm">
                             <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
@@ -55,28 +53,19 @@
                                         <div class="card-item">
                                             <div class="card-item-icon card-icon">
                                                 <svg class="text-primary" xmlns="http://www.w3.org/2000/svg"
-                                                    enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24"
-                                                    width="24">
-                                                    <g>
-                                                        <rect height="14" opacity=".3" width="14" x="5"
-                                                            y="5" />
-                                                        <g>
-                                                            <rect fill="none" height="24" width="24" />
-                                                            <g>
-                                                                <path
-                                                                    d="M19,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V5C21,3.9,20.1,3,19,3z M19,19H5V5h14V19z" />
-                                                                <rect height="5" width="2" x="7"
-                                                                    y="12" />
-                                                                <rect height="10" width="2" x="15"
-                                                                    y="7" />
-                                                                <rect height="3" width="2" x="11"
-                                                                    y="14" />
-                                                                <rect height="2" width="2" x="11"
-                                                                    y="10" />
-                                                            </g>
-                                                        </g>
-                                                    </g>
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                    <rect x="3" y="4" width="18" height="18"
+                                                        rx="2" ry="2"></rect>
+                                                    <line x1="16" y1="2" x2="16" y2="6">
+                                                    </line>
+                                                    <line x1="8" y1="2" x2="8" y2="6">
+                                                    </line>
+                                                    <line x1="3" y1="10" x2="21" y2="10">
+                                                    </line>
                                                 </svg>
+
+
                                             </div>
                                             <div class="card-item-title mb-2">
                                                 <label class="main-content-label tx-13 font-weight-bold mb-1">Meeting
@@ -86,8 +75,7 @@
                                             </div>
                                             <div class="card-item-body">
                                                 <div class="card-item-stat">
-                                                    <h4 class="font-weight-bold">6
-                                                    </h4>
+                                                    <h4 class="font-weight-bold">6</h4>
                                                     <small><b class="text-success">55%</b> higher</small>
                                                 </div>
                                             </div>
@@ -100,20 +88,23 @@
                                     <div class="card-body">
                                         <div class="card-item">
                                             <div class="card-item-icon card-icon">
-                                                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24"
-                                                    width="24">
-                                                    <path d="M0 0h24v24H0V0z" fill="none" />
-                                                    <path
-                                                        d="M12 4c-4.41 0-8 3.59-8 8 0 1.82.62 3.49 1.64 4.83 1.43-1.74 4.9-2.33 6.36-2.33s4.93.59 6.36 2.33C19.38 15.49 20 13.82 20 12c0-4.41-3.59-8-8-8zm0 9c-1.94 0-3.5-1.56-3.5-3.5S10.06 6 12 6s3.5 1.56 3.5 3.5S13.94 13 12 13z"
-                                                        opacity=".3" />
-                                                    <path
-                                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z" />
+                                                <svg class="text-primary" xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                    <rect x="3" y="4" width="18" height="18"
+                                                        rx="2" ry="2"></rect>
+                                                    <line x1="16" y1="2" x2="16" y2="6">
+                                                    </line>
+                                                    <line x1="8" y1="2" x2="8" y2="6">
+                                                    </line>
+                                                    <line x1="3" y1="10" x2="21" y2="10">
+                                                    </line>
                                                 </svg>
                                             </div>
                                             <div class="card-item-title mb-2">
-                                                <label class="main-content-label tx-13 font-weight-bold mb-1">Meetings
+                                                <label class="main-content-label tx-13 font-weight-bold mb-1">Meeting
                                                     Done</label>
-                                                <span class="d-block tx-12 mb-0 text-muted">Employees joined this
+                                                <span class="d-block tx-12 mb-0 text-muted">Meetings done this
                                                     month</span>
                                             </div>
                                             <div class="card-item-body">
@@ -131,15 +122,16 @@
                                     <div class="card-body">
                                         <div class="card-item">
                                             <div class="card-item-icon card-icon">
-                                                <svg class="text-primary" xmlns="http://www.w3.org/2000/svg" height="24"
-                                                    viewBox="0 0 24 24" width="24">
-                                                    <path d="M0 0h24v24H0V0z" fill="none" />
-                                                    <path
-                                                        d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm1.23 13.33V19H10.9v-1.69c-1.5-.31-2.77-1.28-2.86-2.97h1.71c.09.92.72 1.64 2.32 1.64 1.71 0 2.1-.86 2.1-1.39 0-.73-.39-1.41-2.34-1.87-2.17-.53-3.66-1.42-3.66-3.21 0-1.51 1.22-2.48 2.72-2.81V5h2.34v1.71c1.63.39 2.44 1.63 2.49 2.97h-1.71c-.04-.97-.56-1.64-1.94-1.64-1.31 0-2.1.59-2.1 1.43 0 .73.57 1.22 2.34 1.67 1.77.46 3.66 1.22 3.66 3.42-.01 1.6-1.21 2.48-2.74 2.77z"
-                                                        opacity=".3" />
-                                                    <path
-                                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.31-8.86c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
+                                                <svg class="text-primary" xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                    <path d="M0,0h24v24H0V0z" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round"
+                                                        stroke-linejoin="round" />
+                                                    <rect x="4" y="5" width="16" height="16"
+                                                        rx="2" ry="2" />                          
                                                 </svg>
+
                                             </div>
                                             <div class="card-item-title  mb-2">
                                                 <label class="main-content-label tx-13 font-weight-bold mb-1">Issues
@@ -149,8 +141,8 @@
                                             </div>
                                             <div class="card-item-body">
                                                 <div class="card-item-stat">
-                                                    <h4 class="font-weight-bold">{{ App\Models\Issue::count() }}</h4>
-                                                    <small><b class="text-success">15%</b> increase</small>
+                                                    <h4 class="font-weight-bold">8</h4>
+                                                    <small><b class="text-danger">30%</b> lower</small>
                                                 </div>
                                             </div>
                                         </div>
@@ -158,7 +150,10 @@
                                 </div>
                             </div>
                         </div>
-                        <!--End row-->
+                        <!--/Row-->
+
+
+
 
                         <!--row-->
                         <div class="row row-sm">
