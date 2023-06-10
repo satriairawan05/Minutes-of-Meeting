3f57e09cf9493b492c544f1172897281d35c4aef
alsyauqi <ali.subterror@gmail.com>
2023-06-10 10:48:02 +0800

    lupa widget

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -55,7 +55,7 @@
                         <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
                             <div class="card custom-card">
                                 <div class="card-body">
-                                    <div class="card-order ">
+                                    <div class="card-widget ">
                                         <label class="main-content-label mb-3 pt-1">ISSUES HANDLED</label>
                                         <h2 class="text-right card-item-icon card-icon">
                                             <i class="zmdi zmdi-collection-bookmark  icon-size float-left text-primary"></i><span class="font-weight-bold">{{ $countMeet }}</span></h2>
