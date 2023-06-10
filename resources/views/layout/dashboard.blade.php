7a1bab61eee6170c60c95b1f7e9dd1ca975fcf94
alsyauqi <ali.subterror@gmail.com>
2023-06-10 10:09:33 +0800

    sat bangsat toastify

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -1,186 +1,85 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="main-content side-content pt-0">
-        <div class="container-fluid">
-            <div class="inner-body">
+<div class="main-content side-content pt-0">
+    <div class="container-fluid">
+        <div class="inner-body">
 
 
-                <!-- Page Header -->
-                <div class="page-header">
-                    <div>
-                        <h2 class="main-content-title tx-24 mg-b-5">Welcome To Dashboard</h2>
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-                        </ol>
-                    </div>
+            <!-- Page Header -->
+            <div class="page-header">
+                <div>
+                    <h2 class="main-content-title tx-24 mg-b-5">Welcome To Dashboard</h2>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
                 </div>
-                <!-- End Page Header -->
+            </div>
+            <!-- End Page Header -->
 
-                <!--Row-->
-                <div class="row row-sm">
-                    <div class="col-sm-12 col-lg-12">
+            <!--Row-->
+            <div class="row row-sm">
+                <div class="col-sm-12 col-lg-12">
 
-                        <!--Row-->
-                        <div class="row row-sm  mt-lg-4">
-                            <div class="col-sm-12 col-lg-12 col-xl-12">
-                                <div class="card bg-primary custom-card card-box">
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="offset-xl-3 offset-sm-6 col-xl-8 col-sm-6 col-12 img-bg ">
-                                                <h4 class="d-flex  mb-3">
-                                                    <span class="font-weight-bold text-white ">
-                                                        Assalamualaikum {{ auth()->user()->name }}!</span>
-                                                </h4>
-                                                <p class="tx-white-7 mb-1">You have two projects to finish, you
-                                                    had completed <b class="text-warning">57%</b> from your montly
-                                                    level,
-                                                    Keep going to your level
-                                            </div>
-                                            <img src="assets/img/pngs/work3.png" alt="user-img" class="wd-200">
+                    <!--Row-->
+                    <div class="row row-sm  mt-lg-4">
+                        <div class="col-sm-12 col-lg-12 col-xl-12">
+                            <div class="card bg-primary custom-card card-box">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="offset-xl-3 offset-sm-6 col-xl-8 col-sm-6 col-12 img-bg ">
+                                            <h4 class="d-flex  mb-3">
+                                                <span class="font-weight-bold text-white ">
+                                                    Assalamualaikum {{ auth()->user()->name }}!</span>
+                                            </h4>
+                                            <p class="tx-white-7 mb-1">You have two projects to finish, you
+                                                had completed <b class="text-warning">57%</b> from your montly
+                                                level,
+                                                Keep going to your level
                                         </div>
+                                        <img src="assets/img/pngs/work3.png" alt="user-img" class="wd-200">
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!--Row -->
-                        <!--Row-->
-                        <div class="row row-sm">
-                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                                <div class="card custom-card">
-                                    <div class="card-body">
-                                        <div class="card-item">
-                                            <div class="card-item-icon card-icon">
-                                                <svg class="text-primary" xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                    <rect x="3" y="4" width="18" height="18"
-                                                        rx="2" ry="2"></rect>
-                                                    <line x1="16" y1="2" x2="16" y2="6">
-                                                    </line>
-                                                    <line x1="8" y1="2" x2="8" y2="6">
-                                                    </line>
-                                                    <line x1="3" y1="10" x2="21" y2="10">
-                                                    </line>
-                                                </svg>
+                    </div>
+                    <!--Row -->
+                    <!--Row-->
+                    @php
+                    $countMeet = App\Models\Meet::count();
+                    $countIssues = App\Models\Issue::count();
 
-
-                                            </div>
-                                            <div class="card-item-title mb-2">
-                                                <label class="main-content-label tx-13 font-weight-bold mb-1">Meeting
-                                                    Held</label>
-                                                <span class="d-block tx-12 mb-0 text-muted">Previous month vs this
-                                                    months</span>
-                                            </div>
-                                            <div class="card-item-body">
-                                                <div class="card-item-stat">
-                                                    <h4 class="font-weight-bold">6</h4>
-                                                    <small><b class="text-success">55%</b> higher</small>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                                <div class="card custom-card">
-                                    <div class="card-body">
-                                        <div class="card-item">
-                                            <div class="card-item-icon card-icon">
-                                                <svg class="text-primary" xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                    <rect x="3" y="4" width="18" height="18"
-                                                        rx="2" ry="2"></rect>
-                                                    <line x1="16" y1="2" x2="16" y2="6">
-                                                    </line>
-                                                    <line x1="8" y1="2" x2="8" y2="6">
-                                                    </line>
-                                                    <line x1="3" y1="10" x2="21" y2="10">
-                                                    </line>
-                                                </svg>
-                                            </div>
-                                            <div class="card-item-title mb-2">
-                                                <label class="main-content-label tx-13 font-weight-bold mb-1">Meeting
-                                                    Done</label>
-                                                <span class="d-block tx-12 mb-0 text-muted">Meetings done this
-                                                    month</span>
-                                            </div>
-                                            <div class="card-item-body">
-                                                <div class="card-item-stat">
-                                                    <h4 class="font-weight-bold">15</h4>
-                                                    <small><b class="text-success">5%</b> Increased</small>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-4">
-                                <div class="card custom-card">
-                                    <div class="card-body">
-                                        <div class="card-item">
-                                            <div class="card-item-icon card-icon">
-                                                <svg class="text-primary" xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path d="M0,0h24v24H0V0z" fill="none" stroke="currentColor"
-                                                        stroke-width="2" stroke-linecap="round"
-                                                        stroke-linejoin="round" />
-                                                    <rect x="4" y="5" width="16" height="16"
-                                                        rx="2" ry="2" />                          
-                                                </svg>
-
-                                            </div>
-                                            <div class="card-item-title  mb-2">
-                                                <label class="main-content-label tx-13 font-weight-bold mb-1">Issues
-                                                    Handled</label>
-                                                <span class="d-block tx-12 mb-0 text-muted">Previous month vs this
-                                                    months</span>
-                                            </div>
-                                            <div class="card-item-body">
-                                                <div class="card-item-stat">
-                                                    <h4 class="font-weight-bold">8</h4>
-                                                    <small><b class="text-danger">30%</b> lower</small>
-                                                </div>
-                                            </div>
-                                        </div>
+                    @endphp
+                    <!-- Row -->
+                    <div class="row row-sm justify-content-center">
+                        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                            <div class="card custom-card">
+                                <div class="card-body">
+                                    <div class="card-widget ">
+                                        <label class="main-content-label mb-3 pt-1">MEETINGS HELD</label>
+                                        <h2 class="text-right card-item-icon card-icon">
+                                            <i class="zmdi zmdi-collection-text icon-size float-left text-primary"></i><span class="font-weight-bold">{{$countMeet}}</span></h2>
+                                        <p class="mb-0 text-muted">Total of Meetings Handled<span class="float-right"></span></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <!-- COL END -->
+                        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                            <div class="card custom-card">
+                                <div class="card-body">
+                                    <div class="card-widget">
+                                        <label class="main-content-label mb-3 pt-1">ISSUES HANDLED</label>
+                                        <h2 class="text-right"><i class="zmdi zmdi-collection-bookmark icon-size float-left text-primary"></i><span class="font-weight-bold">{{$countIssues}}</span></h2>
+                                        <p class="mb-0 text-muted">Total of Issues Handled<span class="float-right"></span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- COL END -->
+
                         <!--/Row-->
-
-
-
-
-                        <!--row-->
-                        <div class="row row-sm">
-                            <div class="col-sm-12 col-lg-12 col-xl-12">
-                                <div class="card custom-card overflow-hidden">
-                                    <div class="card-header border-bottom-0">
-                                        <div>
-                                            <label class="main-content-label mb-2"></label> <span
-                                                class="d-block tx-12 mb-0 text-muted"></span>
-                                        </div>
-                                    </div>
-                                    <div class="card-body pl-0">
-                                        <div class>
-                                            <div class="container">
-                                                <canvas id="chartLine" class="chart-dropshadow2 ht-250"></canvas>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div><!-- col end -->
-                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                            </div><!-- col end -->
-                            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                            </div><!-- col end -->
-                            <div class="col-lg-12">
-                            </div><!-- col end -->
-                        </div><!-- Row end -->
                     </div><!-- col end -->
                     <!-- col end -->
                 </div>
@@ -191,4 +90,4 @@
 
 
     </div>
-@endsection
+    @endsection
