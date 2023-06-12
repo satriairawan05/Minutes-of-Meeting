9564f27c7554603cd4d790e8d91f5e75d2324219
alsyauqi <ali.subterror@gmail.com>
2023-06-12 11:15:54 +0800

    sat bangsat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,17 +15,29 @@
             <p>After logging in, you can access Minutes of Meeting.</p>
             <form>
                 <div class="form-item">
-                    <input type="text" class="form-element" placeholder="Username or Email">
+                    <input type="text" class="form-element form-control-lg @error('email')is-invalid @enderror" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
                 <div class="form-item">
-                    <input type="password" class="form-element" placeholder="Password">
+                    <input type="password" class="form-element form-control-lg @error('password') is-invalid @enderror" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" name="password" required autocomplete="current-password" />
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
                 <div class="form-checkbox-item">
-                    <input type="checkbox" id="rememberMe" checked>
-                    <label for="rememberMe">Remember Me</label>
+                    <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                    <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember
+                        me</label>
                 </div>
                 <div class="flex">
                     <a href="#">Reset your password now</a>
+                    
                     <button type="button">Sign in</button>
                 </div>
                 <p style="margin-top: 3rem; margin-bottom: 1.5rem;"></p>
@@ -37,6 +49,6 @@
             <a href="#">Sign Up</a>
         </div>
     </div>
-    
+
 </body>
 </html>
