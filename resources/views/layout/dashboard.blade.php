d3df8bb2be5033d30f7ccb5046eb6b256c6f3b77
key <ali.subterror@gmail.com>
2023-06-18 23:44:30 +0800

    greeting card

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -8,9 +8,9 @@
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.6/lottie.min.js"></script>
 
                 <div class="card">
-                    <div class="card-body d-flex align-items-center">
-                        <div class="mr-3" style="width: 50%; height: auto;">
-                            <div id="animationContainer" style="width: 50%; height: 100%;"></div>
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 210px;">
+                        <div class="mr-3" style="width: 40%; height: 100%;">
+                            <div id="animationContainer" style="width: 100%; height: 100%;"></div>
                             <script>
                                 var animation = bodymovin.loadAnimation({
                                     container: document.getElementById('animationContainer'),
@@ -21,14 +21,15 @@
                                 });
                             </script>
                         </div>
-                        <div class="flex-grow-1">
-                            <h4 class="d-flex mb-3">
+                        <div class="flex-grow-1 ml-3">
+                            <h4 class="mb-3">
                                 <span class="font-weight-bold text-white">Assalamualaikum Wr.Wb., {{ auth()->user()->name }}</span>
                             </h4>
                             <p class="tx-black-7 mb-1">You have two projects to finish, you have completed <b class="text-warning">57%</b> of MoM</p>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
         <div class="card radius-10">
@@ -59,7 +60,7 @@
                 <div class="col">
                     <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0">$countIssues</h5>
+                            <h5 class="mb-0">{{$countIssues}}</h5>
                             <div class="ms-auto">
                                 <i class='bx bx-message-alt-dots fs-3 text-white'></i>
                             </div>
@@ -76,7 +77,7 @@
                 <div class="col">
                     <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0">$countDaily</h5>
+                            <h5 class="mb-0">{{$countDaily}}</h5>
                             <div class="ms-auto">
                                 <i class='bx bx-file fs-3 text-white'></i>
                             </div>
@@ -93,7 +94,7 @@
                 <div class="col">
                     <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0">$countArchieve</h5>
+                            <h5 class="mb-0">{{$countArchieve}}</h5>
                             <div class="ms-auto">
                                 <i class='bx bx-folder-open fs-3 text-white'></i>
                             </div>
@@ -109,296 +110,6 @@
                 </div>
             </div><!--end row-->
         </div>
-
-        <div class="row">
-            <div class="col-12 col-lg-8 col-xl-8 d-flex">
-                <div class="card radius-10 w-100">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div>
-                                <h5 class="mb-0">Site Traffic</h5>
-                            </div>
-                            <div class="dropdown options ms-auto">
-                                <div class="dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
-                                    <i class='bx bx-dots-horizontal-rounded'></i>
-                                </div>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="javascript:;">Action</a></li>
-                                    <li><a class="dropdown-item" href="javascript:;">Another action</a></li>
-                                    <li><a class="dropdown-item" href="javascript:;">Something else here</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="d-flex align-items-center ms-auto font-13 gap-2 my-3">
-                            <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1 text-white"></i>New Visitor</span>
-                            <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1 text-light-1"></i>Old Visitor</span>
-                        </div>
-                        <div class="chart-container-1">
-                            <canvas id="chart1"></canvas>
-                        </div>
-                    </div>
-                    <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
-                        <div class="col">
-                            <div class="p-3">
-                                <h5 class="mb-0">45.87M</h5>
-                                <small class="mb-0">Overall Visitor <span> <i class="bx bx-up-arrow-alt align-middle"></i> 2.43%</span></small>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="p-3">
-                                <h5 class="mb-0">15:48</h5>
-                                <small class="mb-0">Visitor Duration <span> <i class="bx bx-up-arrow-alt align-middle"></i> 12.65%</span></small>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="p-3">
-                                <h5 class="mb-0">245.65</h5>
-                                <small class="mb-0">Pages/Visit <span> <i class="bx bx-up-arrow-alt align-middle"></i> 5.62%</span></small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-lg-4 col-xl-4 d-flex">
-
-                <div class="card radius-10 overflow-hidden w-100">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center mb-2">
-                            <div>
-                                <h5 class="mb-0">Weekly sales</h5>
-                            </div>
-                            <div class="dropdown options ms-auto">
-                                <div class="dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
-                                    <i class='bx bx-dots-horizontal-rounded'></i>
-                                </div>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="javascript:;">Action</a></li>
-                                    <li><a class="dropdown-item" href="javascript:;">Another action</a></li>
-                                    <li><a class="dropdown-item" href="javascript:;">Something else here</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="chart-js-container2">
-                            <div class="piechart-legend">
-                                <h3 class="mb-1">95K</h3>
-                                <h6 class="mb-0">Total sales</h6>
-                            </div>
-                            <canvas id="chart2"></canvas>
-                        </div>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table align-items-center mb-0">
-                            <tbody>
-                                <tr class="border-top">
-                                    <td><i class="bx bxs-circle text-white me-2"></i> Direct</td>
-                                    <td>$5856</td>
-                                    <td>+55%</td>
-                                </tr>
-                                <tr>
-                                    <td><i class="bx bxs-circle text-light-2 me-2"></i>Affiliate</td>
-                                    <td>$2602</td>
-                                    <td>+25%</td>
-                                </tr>
-                                <tr>
-                                    <td><i class="bx bxs-circle text-light-3 me-2"></i>E-mail</td>
-                                    <td>$1802</td>
-                                    <td>+15%</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div><!--End Row-->
-
-
-        <div class="row row-cols-1 row-cols-lg-3">
-            <div class="col">
-                <div class="card radius-10">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="w_chart easy-dash-chart1" data-percent="60">
-                                <span class="w_percent"></span>
-                            </div>
-                            <div class="ms-3">
-                                <h6 class="mb-0">Facebook Followers</h6>
-                                <small class="mb-0">22.14% <i class='bx bxs-up-arrow align-middle me-1'></i>Since Last Week</small>
-                            </div>
-                            <div class="ms-auto fs-1 text-white"><i class='bx bxl-facebook'></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card radius-10">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="w_chart easy-dash-chart2" data-percent="65">
-                                <span class="w_percent"></span>
-                            </div>
-                            <div class="ms-3">
-                                <h6 class="mb-0">Twitter Tweets</h6>
-                                <small class="mb-0">32.15% <i class='bx bxs-up-arrow align-middle me-1'></i>Since Last Week</small>
-                            </div>
-                            <div class="ms-auto fs-1 text-white"><i class='bx bxl-twitter'></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card radius-10">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="w_chart easy-dash-chart3" data-percent="75">
-                                <span class="w_percent"></span>
-                            </div>
-                            <div class="ms-3">
-                                <h6 class="mb-0">Youtube Subscribers</h6>
-                                <small class="mb-0">58.24% <i class='bx bxs-up-arrow align-middle me-1'></i>Since Last Week</small>
-                            </div>
-                            <div class="ms-auto fs-1 text-white"><i class='bx bxl-youtube'></i></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--End Row-->
-
-
-        <div class="row">
-            <div class="col-12 col-lg-12 col-xl-6">
-                <div class="card radius-10">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center mb-3">
-                            <div>
-                                <h5 class="mb-0">Selling Region</h5>
-                            </div>
-                            <div class="dropdown options ms-auto">
-                                <div class="dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
-                                    <i class='bx bx-dots-horizontal-rounded'></i>
-                                </div>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="javascript:;">Action</a></li>
-                                    <li><a class="dropdown-item" href="javascript:;">Another action</a></li>
-                                    <li><a class="dropdown-item" href="javascript:;">Something else here</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div id="dashboard-map" style="height: 335px;"></div>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table table-hover align-items-center mb-0">
-                            <thead class="table-light">
-                                <tr>
-                                    <th>Country</th>
-                                    <th>Income</th>
-                                    <th>Trend</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><i class="flag-icon flag-icon-ca me-2"></i> USA</td>
-                                    <td>$4,586</td>
-                                    <td><span id="trendchart1"></span></td>
-                                </tr>
-                                <tr>
-                                    <td><i class="flag-icon flag-icon-us me-2"></i>Canada</td>
-                                    <td>$2,089</td>
-                                    <td><span id="trendchart2"></span></td>
-                                </tr>
-
-                                <tr>
-                                    <td><i class="flag-icon flag-icon-in me-2"></i>India</td>
-                                    <td>$3,039</td>
-                                    <td><span id="trendchart3"></span></td>
-                                </tr>
-
-                                <tr>
-                                    <td><i class="flag-icon flag-icon-gb me-2"></i>UK</td>
-                                    <td>$2,309</td>
-                                    <td><span id="trendchart4"></span></td>
-                                </tr>
-
-                                <tr>
-                                    <td><i class="flag-icon flag-icon-de me-2"></i>Germany</td>
-                                    <td>$7,209</td>
-                                    <td><span id="trendchart5"></span></td>
-                                </tr>
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-lg-12 col-xl-6">
-                <div class="row">
-                    <div class="col-12 col-lg-6">
-                        <div class="card radius-10 overflow-hidden">
-                            <div class="card-body">
-                                <p class="mb-2">Page Views</p>
-                                <h4 class="mb-0">8,293 <small class="font-13 text-white">5.2% <i class="bx bx-up-arrow-alt"></i></small></h4>
-                            </div>
-                            <div class="chart-container-2">
-                                <canvas id="chart3"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-6">
-                        <div class="card radius-10 overflow-hidden">
-                            <div class="card-body">
-                                <p class="mb-2">Total Clicks</p>
-                                <h4 class="mb-0">7,493 <small class="font-13 text-white">1.4% <i class="bx bx-up-arrow-alt"></i></small></h4>
-
-                            </div>
-                            <div class="chart-container-2">
-                                <canvas id="chart4"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-6">
-                        <div class="card radius-10">
-                            <div class="card-body text-center">
-                                <p class="mb-4">Total Downloads</p>
-                                <input class="knob" data-width="190" data-height="190" data-readOnly="true" data-thickness=".15" data-angleoffset="90" data-linecap="round" data-bgcolor="rgba(0, 0, 0, 0.08)" data-fgcolor="#fff" data-max="15000" value="8550" />
-                                <hr>
-                                <p class="mb-0 small-font text-center">3.4% <i class="zmdi zmdi-long-arrow-up"></i> since yesterday</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-6">
-                        <div class="card radius-10">
-                            <div class="card-body">
-                                <p>Device Storage</p>
-                                <h4 class="mb-3">42620/50000</h4>
-                                <hr>
-                                <div class="progress-wrapper mb-4">
-                                    <p>Documents <span class="float-end">12GB</span></p>
-                                    <div class="progress" style="height:5px;">
-                                        <div class="progress-bar bg-white" style="width:80%"></div>
-                                    </div>
-                                </div>
-
-                                <div class="progress-wrapper mb-4">
-                                    <p>Images <span class="float-end">10GB</span></p>
-                                    <div class="progress" style="height:5px;">
-                                        <div class="progress-bar bg-white" style="width:60%"></div>
-                                    </div>
-                                </div>
-
-                                <div class="progress-wrapper mb-4">
-                                    <p>Mails <span class="float-end">5GB</span></p>
-                                    <div class="progress" style="height:5px;">
-                                        <div class="progress-bar bg-white" style="width:40%"></div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--End Row-->
 
         <div class="row">
             <div class="col-12 col-lg-6 col-xl-4 d-flex">
