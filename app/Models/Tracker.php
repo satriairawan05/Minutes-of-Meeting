86488c581b4b34e037cf20b9e70613bc748bb8f0
key <ali.subterror@gmail.com>
2023-06-19 17:10:56 +0800

    bgst

---
+++ app/Models/Tracker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tracker extends Model
+{
+    use HasFactory;
+
+    protected $table = 'daily_trackers';
+    protected $primaryKey = 'tracker_id';
+}
