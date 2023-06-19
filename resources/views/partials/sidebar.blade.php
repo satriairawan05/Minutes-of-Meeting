839c52cd318f3b4f1d0ed719af1ad6dae8aad05d
Satria Irawan <satriai418@gmail.com>
2023-06-19 08:46:44 +0800

    Revert "Migrate Sidebar"
    This reverts commit 920d51043b251a5fa718fa80187995a7a70b15b1.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -36,62 +36,131 @@ $pages = App\Models\User::leftJoin('group_pages', 'users.group_id', '=', 'group_
         <!--navigation-->
         <ul class="metismenu" id="menu">
             <li>
-            <a class="nav-link {{ request()->is('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">
-                    <div class="parent-icon"><i class='bx bx-home'></i>
+                <a href="widgets.html">
+                    <div class="parent-icon"><i class='bx bx-cookie'></i>
                     </div>
-                    <div class="menu-title">Dashboard</div>
+                    <div class="menu-title">Widgets</div>
                 </a>
             </li>
             <li>
-            <a class="nav-link {{ request()->is('meet.index', 'meet.create') ? 'active' : '' }}" href="{{ route('meet.index') }}">
-                    <div class="parent-icon"><i class='bx bx-calendar'></i>
+                <a href="javascript:;" class="has-arrow">
+                    <div class="parent-icon"><i class='bx bx-cart'></i>
                     </div>
-                    <div class="menu-title">{{ $Meet }}</div>
+                    <div class="menu-title">eCommerce</div>
                 </a>
+                <ul>
+                    <li> <a href="ecommerce-products.html"><i class='bx bx-radio-circle'></i>Products</a>
+                    </li>
+                    <li> <a href="ecommerce-products-details.html"><i class='bx bx-radio-circle'></i>Product Details</a>
+                    </li>
+                    <li> <a href="ecommerce-add-new-products.html"><i class='bx bx-radio-circle'></i>Add New Products</a>
+                    </li>
+                    <li> <a href="ecommerce-orders.html"><i class='bx bx-radio-circle'></i>Orders</a>
+                    </li>
+                </ul>
             </li>
             <li>
-            <a class="nav-link {{ request()->is('issue.index') ? 'active' : '' }}" href="{{ route('issue.index') }}">
-                    <div class="parent-icon"><i class='bx bx-home'></i>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
                     </div>
-                    <div class="menu-title">{{ $Issue }}</div>
+                    <div class="menu-title">Components</div>
                 </a>
+                <ul>
+                    <li> <a href="component-alerts.html"><i class='bx bx-radio-circle'></i>Alerts</a>
+                    </li>
+                    <li> <a href="component-accordions.html"><i class='bx bx-radio-circle'></i>Accordions</a>
+                    </li>
+                    <li> <a href="component-badges.html"><i class='bx bx-radio-circle'></i>Badges</a>
+                    </li>
+                    <li> <a href="component-buttons.html"><i class='bx bx-radio-circle'></i>Buttons</a>
+                    </li>
+                    <li> <a href="component-cards.html"><i class='bx bx-radio-circle'></i>Cards</a>
+                    </li>
+                    <li> <a href="component-carousels.html"><i class='bx bx-radio-circle'></i>Carousels</a>
+                    </li>
+                    <li> <a href="component-list-groups.html"><i class='bx bx-radio-circle'></i>List Groups</a>
+                    </li>
+                    <li> <a href="component-media-object.html"><i class='bx bx-radio-circle'></i>Media Objects</a>
+                    </li>
+                    <li> <a href="component-modals.html"><i class='bx bx-radio-circle'></i>Modals</a>
+                    </li>
+                    <li> <a href="component-navs-tabs.html"><i class='bx bx-radio-circle'></i>Navs & Tabs</a>
+                    </li>
+                    <li> <a href="component-navbar.html"><i class='bx bx-radio-circle'></i>Navbar</a>
+                    </li>
+                    <li> <a href="component-paginations.html"><i class='bx bx-radio-circle'></i>Pagination</a>
+                    </li>
+                    <li> <a href="component-popovers-tooltips.html"><i class='bx bx-radio-circle'></i>Popovers & Tooltips</a>
+                    </li>
+                    <li> <a href="component-progress-bars.html"><i class='bx bx-radio-circle'></i>Progress</a>
+                    </li>
+                    <li> <a href="component-spinners.html"><i class='bx bx-radio-circle'></i>Spinners</a>
+                    </li>
+                    <li> <a href="component-notifications.html"><i class='bx bx-radio-circle'></i>Notifications</a>
+                    </li>
+                    <li> <a href="component-avtars-chips.html"><i class='bx bx-radio-circle'></i>Avatrs & Chips</a>
+                    </li>
+                </ul>
             </li>
             <li>
-            <a class="nav-link {{ request()->is('issue.index') ? 'active' : '' }}" href="{{ route('daily.index') }}">
-                    <div class="parent-icon"><i class='bx bx-home'></i>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class="bx bx-repeat"></i>
                     </div>
-                    <div class="menu-title">{{ str_replace('_', ' ', $Daily) }}</div>
+                    <div class="menu-title">Content</div>
                 </a>
+                <ul>
+                    <li> <a href="content-grid-system.html"><i class='bx bx-radio-circle'></i>Grid System</a>
+                    </li>
+                    <li> <a href="content-typography.html"><i class='bx bx-radio-circle'></i>Typography</a>
+                    </li>
+                    <li> <a href="content-text-utilities.html"><i class='bx bx-radio-circle'></i>Text Utilities</a>
+                    </li>
+                </ul>
             </li>
             <li>
-            <a class="nav-link {{ request()->is('archive.index') ? 'active' : '' }}" href="{{ route('archive.index') }}">
-                    <div class="parent-icon"><i class='bx bx-home'></i>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"> <i class="bx bx-donate-blood"></i>
                     </div>
-                    <div class="menu-title">{{ $Archive }}</div>
+                    <div class="menu-title">Icons</div>
                 </a>
-            </li>
-            <a class="nav-link {{ request()->is('user.index') ? 'active' : '' }}" href="{{ route('user.index') }}">
-                    <div class="parent-icon"><i class='bx bx-home'></i>
-                    </div>
-                    <div class="menu-title">{{ $User }}</div>
-                </a>
+                <ul>
+                    <li> <a href="icons-line-icons.html"><i class='bx bx-radio-circle'></i>Line Icons</a>
+                    </li>
+                    <li> <a href="icons-boxicons.html"><i class='bx bx-radio-circle'></i>Boxicons</a>
+                    </li>
+                    <li> <a href="icons-feather-icons.html"><i class='bx bx-radio-circle'></i>Feather Icons</a>
+                    </li>
+                </ul>
             </li>
 
-
-            <li class="menu-label">Manage</li>
+            <li class="menu-label">Forms & Tables</li>
             <li>
                 <a class="has-arrow" href="javascript:;">
                     <div class="parent-icon"><i class='bx bx-message-square-edit'></i>
                     </div>
-                    <div class="menu-title">Access</div>
+                    <div class="menu-title">Forms</div>
                 </a>
                 <ul>
-                    <li> <a href="{{ route('departemen.index') }}"><i class='bx bx-radio-circle'></i>Department</a>
+                    <li> <a href="form-elements.html"><i class='bx bx-radio-circle'></i>Form Elements</a>
                     </li>
-                    <li> <a href="{{ route('group.index') }}"><i class='bx bx-radio-circle'></i>Roles</a>
+                    <li> <a href="form-input-group.html"><i class='bx bx-radio-circle'></i>Input Groups</a>
                     </li>
 
-                    <li> <a href="#"><i class='bx bx-radio-circle'></i>DWM Tracker</a>
+                    <li> <a href="form-layouts.html"><i class='bx bx-radio-circle'></i>Forms Layouts</a>
+                    </li>
+                    <li> <a href="form-validations.html"><i class='bx bx-radio-circle'></i>Form Validation</a>
+                    </li>
+                    <li> <a href="form-wizard.html"><i class='bx bx-radio-circle'></i>Form Wizard</a>
+                    </li>
+                    <li> <a href="form-text-editor.html"><i class='bx bx-radio-circle'></i>Text Editor</a>
+                    </li>
+                    <li> <a href="form-file-upload.html"><i class='bx bx-radio-circle'></i>File Upload</a>
+                    </li>
+                    <li> <a href="form-date-time-pickes.html"><i class='bx bx-radio-circle'></i>Date Pickers</a>
+                    </li>
+                    <li> <a href="form-select2.html"><i class='bx bx-radio-circle'></i>Select2</a>
+                    </li>
+                    <li> <a href="form-repeater.html"><i class='bx bx-radio-circle'></i>Form Repeater</a>
                     </li>
                 </ul>
             </li>
