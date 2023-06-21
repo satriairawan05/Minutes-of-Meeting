ea0c24aa195bfe2a05cdf3b365183f059b01e62a
key <ali.subterror@gmail.com>
2023-06-21 14:38:48 +0800

    dwms

--- resources/views/daily/index.blade.php
+++ resources/views/daily/index.blade.php
@@ -52,7 +52,7 @@ $delete = $page->access;
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a></li>
-                        <li class="breadcrumb-item" aria-current="page">Datatable of DWM Report</li>
+                        <li class="breadcrumb-item" aria-current="page">Data of DWM Report by Department</li>
                     </ol>
                 </nav>
             </div>
--- resources/views/daily/index_3.blade.php
+++ resources/views/daily/index_3.blade.php
@@ -6,55 +6,64 @@ $create = 0;
 $read = 0;
 $update = 0;
 $delete = 0;
+$approval = 0;
+$create = 0;
+$read = 0;
+$update = 0;
+$delete = 0;
 @endphp
 
 @foreach ($pages as $page)
-@if($page->action == "Approval")
-@php
-$approval = $page->access;
-@endphp
-@endif
+    @if($page->action == "Approval")
+        @php
+        $approval = $page->access;
+        @endphp
+    @endif
 
-@if($page->action == "Create")
-@php
-$create = $page->access;
-@endphp
-@endif
+    @if($page->action == "Create")
+        @php
+        $create = $page->access;
+        @endphp
+    @endif
 
-@if($page->action == "Read")
-@php
-$read = $page->access;
-@endphp
-@endif
+    @if($page->action == "Read")
+        @php
+        $read = $page->access;
+        @endphp
+    @endif
 
-@if($page->action == "Update")
-@php
-$update = $page->access;
-@endphp
-@endif
+    @if($page->action == "Update")
+        @php
+        $update = $page->access;
+        @endphp
+    @endif
 
-@if($page->action == "Delete")
-@php
-$delete = $page->access;
-@endphp
-@endif
+    @if($page->action == "Delete")
+        @php
+        $delete = $page->access;
+        @endphp
+    @endif
 @endforeach
 
 @section('content')
-<link href="{{ asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" />
-<div class="page-wrapper">
-    <div class="page-content">
-        <div class="inner-body">
-            <!-- Page Header -->
-            <div class="page-header">
-                <div>
-                    <h2 class="main-content-title tx-24 mg-b-5">DWM Report</h2>
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item" aria-current="page">DWM Report</li>
-                    </ol>
+    <!-- Start page wrapper -->
+    <div class="page-wrapper">
+        <div class="page-content">
+            <!-- Breadcrumb -->
+            <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+                <div class="breadcrumb-title pe-3">DWM Report</div>
+                <div class="ps-3">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0 p-0">
+                            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a></li>
+                            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-file"></i></a></li>
+                            <li class="breadcrumb-item" aria-current="page">Data Tracker</li>
+                        </ol>
+                    </nav>
                 </div>
             </div>
+            <!-- End breadcrumb -->
+            <hr />
             <!-- End Page Header -->
             <div class="card">
                 <div class="card-header">
