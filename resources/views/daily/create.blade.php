d5b109a81c587912cbcf5d428b6bd56b8129a669
Satria Irawan <satriai418@gmail.com>
2023-06-22 15:34:58 +0800

    fix form

--- resources/views/daily/create.blade.php
+++ resources/views/daily/create.blade.php
@@ -32,7 +32,7 @@
                         </label>
                     </div>
                     <div class="row mb-3">
-                        <div class="col-md-6">
+                        <div class="col-6">
                             <label for="daily_xid">ID Daily</label>
                             <input type="text" class="form-control @error('daily_xid') is-invalid @enderror" id="daily_xid" name="daily_xid" value="{{ $daily }}" readonly>
                             @error('daily_xid')
@@ -41,32 +41,32 @@
                             </div>
                             @enderror
                         </div>
-                            <div class="col-md-6">
-                                <label id="subject_label" for="subject">Subject</label>
-                                <input id="subject" name="subject" type="text" class="form-control @error('subject') is_invalid @enderror" required placeholder="Masukan Subject" value="{{ old('subject') }}" />
-                                @error('subject')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                        <div class="col-6">
+                            <label id="subject_label" for="subject">Subject</label>
+                            <input id="subject" name="subject" type="text" class="form-control @error('subject') is_invalid @enderror" required placeholder="Masukan Subject" value="{{ old('subject') }}" />
+                            @error('subject')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
+                            @enderror
                         </div>
-                        <div class="row mb-3">
-                            @php
-                            $statuses = array("New","Continue","Over Due","Complete","Closed")
-                            @endphp
-                            <div class="col-md-6">
-                                <label id="status_label" for="status">Status</label>
-                                <select class="form-select form-control form-control-sm" name="status">
-                                    @foreach ($statuses as $status)
-                                    @if (old('status') == $status)
-                                    <option name="status" value="{{ $status }}" selected>{{ $status }}</option>
-                                    @else
-                                    <option name="status" value="{{ $status }}">{{ $status }}</option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                            </div>
+                    </div>
+                    <div class="row mb-3">
+                        @php
+                        $statuses = array("New","Continue","Over Due","Complete","Closed")
+                        @endphp
+                        <div class="col-6">
+                            <label id="status_label" for="status">Status</label>
+                            <select class="form-select form-control form-control-sm" name="status">
+                                @foreach ($statuses as $status)
+                                @if (old('status') == $status)
+                                <option name="status" value="{{ $status }}" selected>{{ $status }}</option>
+                                @else
+                                <option name="status" value="{{ $status }}">{{ $status }}</option>
+                                @endif
+                                @endforeach
+                            </select>
+                        </div>
                         <div class="col-6">
                             <label id="priority_label" for="priority">Priority</label>
                             <select class="form-select form-control form-control-sm" name="priority">
@@ -117,7 +117,7 @@
                             @enderror
                         </div>
                     </div>
-                    <div class="mb-3 col-13">
+                    <div class="mb-3 col-12">
                         <label id="description_label" for="description_daily">Problem Identification</label>
                         <textarea type="text" name="description_daily" id="description_daily" value="{{ old('description_daily') }}" class="form-control @error('description') is-invalid @enderror" placeholder="Masukan Description"></textarea>
                         @error('description_daily')
@@ -126,7 +126,7 @@
                         </div>
                         @enderror
                     </div>
-                    <div class="mb-3 col-13">
+                    <div class="mb-3 col-12">
                         <label id="c_action_label" for="c_action">Corrective Action</label>
                         <textarea id="c_action" name="c_action" rows="14" type="text" class="form-control @error('c_action') is-invalid @enderror" value="{{ old('c_action') }}" placeholder="Masukan Corrective Action"></textarea>
                         @error('c_action')
@@ -136,7 +136,7 @@
                         @enderror
                     </div>
                     <div class="row mb-3">
-                        <div class="col-md-6">
+                        <div class="col-6">
                             <label id="start_date_label" for="start_date">Start Date</label>
                             <input id="start_date" name="start_date" type="date" class="form-control @error('start_date') is_invalid @enderror" required value="{{ old('start_date') }}" placeholder="dd/mm/yyyy" />
                             @error('start_date')
@@ -145,7 +145,7 @@
                             </div>
                             @enderror
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-6">
                             <label id="end_date_label" for="end_date">End Date</label>
                             <input id="end_date" name="end_date" type="date" class="form-control @error('end_date') is_invalid @enderror" required value="{{ old('end_date') }}" placeholder="dd/mm/yyyy" />
                             @error('end_date')
@@ -154,95 +154,98 @@
                             </div>
                             @enderror
                         </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label id="assignee_label" for="assignee">Assignee</label>
-                                <select class="form-select form-control form-control-sm" id="assignee" name="assignee">
-                                    @foreach ($users as $user)
-                                    @if (old('assignee') == $user->name)
-                                    <option name="assignee" value="{{ $user->name }}" selected>
-                                        {{ $user->name }}</option>
-                                    @else
-                                    <option name="assignee" value="{{ $user->name }}">{{ $user->name }}
-                                    </option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                                @error('assignee')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label id="assignee_label" for="assignee">Assignee</label>
+                            <select class="form-select form-control form-control-sm" id="assignee" name="assignee">
+                                @foreach ($users as $user)
+                                @if (old('assignee') == $user->name)
+                                <option name="assignee" value="{{ $user->name }}" selected>
+                                    {{ $user->name }}</option>
+                                @else
+                                <option name="assignee" value="{{ $user->name }}">{{ $user->name }}
+                                </option>
+                                @endif
+                                @endforeach
+                            </select>
+                            @error('assignee')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
-                            <div class="col-md-6">
-                                <label id="pic_label" for="pic">PIC</label>
-                                <select class="form-select form-control form-control-sm" id="pic" name="pic">
-                                    @foreach ($users as $user)
-                                    @if (old('pic') == $user->name)
-                                    <option name="pic" value="{{ $user->name }}" selected>
-                                        {{ $user->name }}</option>
-                                    @else
-                                    <option name="pic" value="{{ $user->name }}">{{ $user->name }}
-                                    </option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                                @error('assignee')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
-                        <div class="row mb-3">
-                            <div class="col-12">
-                                <label id="file_label" for="file">File</label>
-                                <div id="targetLayer"></div>
-                                <div class="icon-choose-image"></div>
-                                <input id="file" name="file" type="file" class="form-control form-control-file @error('file') is_invalid @enderror" value="{{ old('file') }}" onchange="return showPreview(this)" />
-                                @error('file')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                        <div class="col-6">
+                            <label id="pic_label" for="pic">PIC</label>
+                            <select class="form-select form-control form-control-sm" id="pic" name="pic">
+                                @foreach ($users as $user)
+                                @if (old('pic') == $user->name)
+                                <option name="pic" value="{{ $user->name }}" selected>
+                                    {{ $user->name }}</option>
+                                @else
+                                <option name="pic" value="{{ $user->name }}">{{ $user->name }}
+                                </option>
+                                @endif
+                                @endforeach
+                            </select>
+                            @error('assignee')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
+                            @enderror
                         </div>
-                            <div class="col-12 d-flex justify-content-center align-items-center">
-                                <a href="{{ route('daily.index') }}" class="btn btn-md btn-primary mr-3">Back</a>
-                                <button type="submit" class="btn btn-md btn-success">Submit</button>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <label id="file_label" for="file">File</label>
+                            <div id="targetLayer"></div>
+                            <div class="icon-choose-image"></div>
+                            <input id="file" name="file" type="file" class="form-control form-control-file @error('file') is_invalid @enderror" value="{{ old('file') }}" onchange="return showPreview(this)" />
+                            @error('file')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
-                    </form>
-                </div>
+                            @enderror
+                        </div>
+                    </div>
             </div>
         </div>
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <a href="{{ route('daily.index') }}" class="btn btn-md btn-primary mr-3">Back</a>
+            <button type="submit" class="btn btn-md btn-success">Submit</button>
+        </div>
+        </form>
+    </div>
+</div>
+</div>
 
-    <script>
-        $(document).ready(function() {
-            // Hide and Show Columns
-            $('#toggleColumns').on('change', function() {
-                var column = $(this).attr('id');
-                $('.' + column).toggle();
-            });
+<script>
+    $(document).ready(function() {
+        // Hide and Show Columns
+        $('#toggleColumns').on('change', function() {
+            var column = $(this).attr('id');
+            $('.' + column).toggle();
+        });
 
-                // Expandable Columns
-                $('.expandable-column').on('click', function() {
-                    $(this).toggleClass('expanded');
-                    $(this).siblings('.expand-content').toggle();
-                });
-            });
+        // Expandable Columns
+        $('.expandable-column').on('click', function() {
+            $(this).toggleClass('expanded');
+            $(this).siblings('.expand-content').toggle();
+        });
+    });
 
-        </script>
+</script>
 
-        <script>
-            $(document).ready(function() {
-                // Hide and Show Columns
-                $('#toggleColumns').on('change', function() {
-                    var column = $(this).val();
-                    $('.' + column).toggle();
-                });
-            });
+<script>
+    $(document).ready(function() {
+        // Hide and Show Columns
+        $('#toggleColumns').on('change', function() {
+            var column = $(this).val();
+            $('.' + column).toggle();
+        });
+    });
 
-        </script>
+</script>
 
 
 </div>
@@ -270,5 +273,6 @@
             fileReader.readAsDataURL(objFileInput.files[0]);
         }
     }
+
 </script>
 @endsection
