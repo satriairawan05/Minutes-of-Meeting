2e81b473ae76e92af743ba3a4400c6718dbd39e6
key <ali.subterror@gmail.com>
2023-06-22 13:45:48 +0800

    bgsat juga ya

--- resources/views/daily/approved.blade.php
+++ resources/views/daily/approved.blade.php
@@ -11,7 +11,8 @@
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a></li>
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-file"></i></a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Add Approval for DWM Report</li>
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-file-find"></i></a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Approval for DWM Report</li>
                     </ol>
                 </nav>
             </div>
