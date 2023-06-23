c99e45e7a3a9327be1036c6e27495938c1330503
key <ali.subterror@gmail.com>
2023-06-23 15:48:07 +0800

    fix link

--- resources/views/daily/index.blade.php
+++ resources/views/daily/index.blade.php
@@ -51,7 +51,7 @@ $delete = $page->access;
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
-                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><i class="bx bx-home-alt"></i></a></li>
                         <li class="breadcrumb-item" aria-current="page">Data of DWM Report by Department</li>
                     </ol>
                 </nav>
